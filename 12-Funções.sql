6b84d4033348379c2e3117c5c8890336aa01b43c
Max Ramon <MaxRamonNB@Gmail.com>
2025-08-12 13:52:55 -0300

    Update 12-Funções.sql
    Criação da função de ocupação média

--- 12-Funções.sql
+++ 12-Funções.sql
@@ -31,3 +31,29 @@ END$$
 DELIMITER ;
 
 SELECT MediaAvalicoes();
+
+
+DELIMITER $$
+
+CREATE FUNCTION CalcularOcupacaoMedia() RETURNS DECIMAL(5,2)
+DETERMINISTIC
+BEGIN
+    DECLARE totalHospedagens INT;
+    DECLARE totalOcupadas INT;
+    DECLARE ocupacaoMedia DECIMAL(5,2);
+
+    -- Calcula o número total de hospedagens listadas
+    SELECT COUNT(*) INTO totalHospedagens FROM hospedagens;
+    
+    -- Calcula o número total de hospedagens ocupadas
+    SELECT COUNT(*) INTO totalOcupadas FROM alugueis;
+
+    -- Calcula a ocupação média como um percentual
+    SET ocupacaoMedia = (totalOcupadas / totalHospedagens) * 100;
+
+    RETURN ocupacaoMedia;
+END$$
+
+DELIMITER ;
+
+SELECT CalcularOcupacaoMedia();
