a5b09266a2cdf441eefc5a1ccf68b2d8770401ae
Max Ramon <MaxRamonNB@Gmail.com>
2025-08-12 14:23:30 -0300

    Update 12-Funções.sql
    Criação uma função para a formatação do CPF dos clientes

--- 12-Funções.sql
+++ 12-Funções.sql
@@ -57,3 +57,20 @@ END$$
 DELIMITER ;
 
 SELECT CalcularOcupacaoMedia();
+
+
+DELIMITER $$
+CREATE FUNCTION FormatandoCPF (ClienteID INT)
+RETURNS VARCHAR(50) DETERMINISTIC
+BEGIN
+DECLARE NovoCPF VARCHAR(50);
+
+SET NovoCPF = (
+SELECT CONCAT(SUBSTRING (cpf, 1, 3), '.', SUBSTRING(cpf, 4, 3), '.', SUBSTRING(cpf, 7, 3), '-', SUBSTRING(cpf, 10, 2)) AS CPF_Mascarado
+FROM clientes
+WHERE cliente_id = ClienteID
+);
+
+RETURN NovoCPF;
+END$$
+DELIMITER ;
