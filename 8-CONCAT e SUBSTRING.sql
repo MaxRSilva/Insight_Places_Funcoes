ced5dc676113165d091f7c921eb3557b237b9bfe
Max Ramon <MaxRamonNB@Gmail.com>
2025-08-12 12:15:17 -0300

    Create 8-CONCAT e SUBSTRING.sql

---
+++ 8-CONCAT e SUBSTRING.sql
@@ -0,0 +1,9 @@
+SELECT
+  CONCAT(SUBSTRING(cpf, 1, 3), '.', SUBSTRING(cpf, 4, 3), '.', SUBSTRING(cpf, 7, 3), '-', SUBSTRING(cpf, 10, 2)) AS CPF_Mascarado
+FROM
+  clientes;
+  SELECT
+  TRIM(nome) Nome,
+  CONCAT(SUBSTRING(cpf, 1, 3), '.', SUBSTRING(cpf, 4, 3), '.', SUBSTRING(cpf, 7, 3), '-', SUBSTRING(cpf, 10, 2)) AS CPF_Mascarado
+FROM
+  clientes;
