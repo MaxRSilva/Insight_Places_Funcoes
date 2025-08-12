13b3eaafec5b5201abcc2d755eeaabcb13a696ce
Max Ramon <MaxRamonNB@Gmail.com>
2025-08-12 12:40:45 -0300

    Update 10-Data DIFF.sql
    Uso de data DIFF para saber a diferença por tipo de hospedagem

--- 10-Data DIFF.sql
+++ 10-Data DIFF.sql
@@ -10,3 +10,9 @@ SELECT TRIM(nome) AS Nome, DATEDIFF(data_fim, data_inicio) AS TotalDias
 FROM alugueis a
 JOIN clientes c
 ON a.cliente_id = c.cliente_id;
+
+SELECT tipo, DATEDIFF(data_fim, data_inicio) AS TotalDias
+FROM alugueis a
+JOIN hospedagens h
+ON a.hospedagem_id = h.hospedagem_id
+GROUP BY tipo;
