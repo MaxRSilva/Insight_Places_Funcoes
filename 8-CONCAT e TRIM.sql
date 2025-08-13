e2b3988e72f841d125286321f5f365e2182f52da
Max Ramon <MaxRamonNB@Gmail.com>
2025-08-13 11:09:55 -0300

    Update 8-CONCAT e TRIM.sql

--- 8-CONCAT e TRIM.sql
+++ 8-CONCAT e TRIM.sql
@@ -7,3 +7,5 @@ SELECT CONCAT(nome, contato) FROM clientes;
 SELECT CONCAT(nome, ' O email é: ' , contato) FROM clientes;
 
 SELECT CONCAT(TRIM(nome), ' O email é: ' , contato) NomeContato FROM clientes;
+
+
